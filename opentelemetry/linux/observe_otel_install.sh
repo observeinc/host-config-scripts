94964f45879901c466e59378e5aeb1e17b08f51f
Arthur Dayton <103078673+arthur-observe@users.noreply.github.com>
2024-01-22 16:47:49 -0800

    Update observe_otel_install.sh

--- opentelemetry/linux/observe_otel_install.sh
+++ opentelemetry/linux/observe_otel_install.sh
@@ -120,7 +120,7 @@ for url in "${config_urls[@]}"; do
 done
 
 
-echo "OBSERVE_COLLECTION_ENDPOINT=$(echo "$OBSERVE_COLLECTION_ENDPOINT" | sed 's/\/\?$//')" | sudo tee -a "$env_file" >> /dev/null
+echo "OBSERVE_COLLECTION_ENDPOINT=$(echo "$OBSERVE_COLLECTION_ENDPOINT" | sed 's/\/\?$//')" | sudo tee "$env_file" >> /dev/null
 echo "OBSERVE_TOKEN=$OBSERVE_TOKEN" | sudo tee -a "$env_file" >> /dev/null
 
 sudo systemctl enable otelcol-contrib
